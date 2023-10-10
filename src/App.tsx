9bf3d8bbf7b0addfbba3b67f2569d287863e9f83
“Martin <BulikopM@gmail.com>
2023-10-10 17:18:21 +0200

    base api hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable prettier/prettier */
 
-import React, { useState} from 'react';
+import React, { useEffect, useRef, useState} from 'react';
 import {
   Dimensions,
+  DrawerLayoutAndroid,
   Pressable,
   SafeAreaView,
   StyleSheet,
@@ -47,10 +49,25 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: 'lightblue',
   },
+  test: {
+    backgroundColor: 'black',
+    display: 'flex',
+    gap: 10,
+    width: Dimensions.get('screen').width - 80,
+    height: Dimensions.get('screen').height,
+    color: 'black',
+  },
+  tmp: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
 });
 
 function App(): JSX.Element {
+  const drawer = useRef<DrawerLayoutAndroid>(null);
   const [hej, setHej] = useState<any>(0);
+  const [memory, setMemory] = useState<any>([]);
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -90,7 +107,7 @@ function App(): JSX.Element {
       method: "GET",
     });
     let data: any = await raw.json();
-    if (data.count) {
+    if (data) {
       let date = new Date(data.createdAt);
       setHej(Number(data.count));
       Message(`Loaded count ${data.count} from \n ${date.toDateString()}`);
@@ -102,45 +119,93 @@ function App(): JSX.Element {
     Message('Count has been cleared');
   }
 
+  const navigationView = () => (
+    <View style={styles.test}>
+      {memory.map((row) : any => {
+        return (
+          <Pressable
+          key={row.id}
+          onPress={() => {
+            console.log(row.id);
+          }}
+          >
+            <View style={styles.tmp}>
+              <Text>{new Date(row.createdAt).toDateString()}</Text>
+              <Text>{row.count}</Text>
+              <Text style={styles.time}>{new Date(row.createdAt).toTimeString()}</Text>
+            </View>
+          </Pressable>
+        );
+      })
+      }
+    </View>
+  );
+
+
+  useEffect(() => {
+    fetch(`${API_URL}/api/hey`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization : `Bearer ${API_TOKEN}`,
+      },
+      method: "PUT",
+    })
+    .catch(e => {
+      console.debug(e);
+    }).then(raw => raw.json())
+    .then((data) => {
+      setMemory([...data]);      
+    })
+
+    
+  }, [memory])
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <View style={[Components.header, Flex.end]}>
-        <Button style={[Types.secondary, Sizes.smaller, Components.button]} onPress={() => save()}>
-          save
-        </Button>
-      </View>
-      <View>
-        <Text style={styles.title}> THE ULTIMATE 'HEJ'! COUNTER </Text>
-      </View>
-      <View style={styles.count}>
-          <Text style={styles.countText}>
-            {hej}
-          </Text>
-      </View>
-      <View style={styles.buttonView}>
-          <Pressable
-          onPress={() => {setHej(hej+1)}}
-          >
-            <Text style={[Types.primary, Sizes.x_large, Components.button]}>
-              Add to count
+    <DrawerLayoutAndroid
+    ref={drawer}
+    drawerWidth={Dimensions.get('screen').width - 80}
+    drawerPosition='left'
+    renderNavigationView={navigationView}
+    >
+      <SafeAreaView style={backgroundStyle}>
+        <View style={[Components.header, Flex.end]}>
+          <Button style={[Types.secondary, Sizes.smaller, Components.button]} onPress={() => save()}>
+            save
+          </Button>
+        </View>
+        <View>
+          <Text style={styles.title}> THE ULTIMATE 'HEJ'! COUNTER </Text>
+        </View>
+        <View style={styles.count}>
+            <Text style={styles.countText}>
+              {hej}
             </Text>
-          </Pressable>
-          <Pressable
-          onPress={() => {load()}}
-          >
-            <Text style={[Types.warning, Sizes.medium, Components.button]}>
-              load
-            </Text>
-          </Pressable>
-          <Pressable
-          onPress={() => {clean()}}
-          >
-            <Text style={[Types.danger, Sizes.medium, Components.button]}>
-              Clear
-            </Text>
-          </Pressable>
-      </View>
-    </SafeAreaView>
+        </View>
+        <View style={styles.buttonView}>
+            <Pressable
+            onPress={() => {setHej(hej+1)}}
+            >
+              <Text style={[Types.primary, Sizes.x_large, Components.button]}>
+                Add to count
+              </Text>
+            </Pressable>
+            <Pressable
+            onPress={() => {load()}}
+            >
+              <Text style={[Types.warning, Sizes.medium, Components.button]}>
+                load latest
+              </Text>
+            </Pressable>
+            <Pressable
+            onPress={() => {clean()}}
+            >
+              <Text style={[Types.danger, Sizes.medium, Components.button]}>
+                Clear
+              </Text>
+            </Pressable>
+        </View>
+      </SafeAreaView>
+    </DrawerLayoutAndroid>
   );
 }
 export default App;
